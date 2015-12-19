57d2497a362519417c751661197e5d59112a08d0
kangkyu <kangkyu1111@gmail.com>
2015-12-19 11:42:14 -0800

    new action - 'todo new new_task'

--- lib/todo.rb
+++ lib/todo.rb
@@ -2,3 +2,11 @@ require 'todo/version.rb'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
+
+def read_todo(line)
+  line.chomp.split(/,/)
+end
+
+def write_todo(file,name,created,completed)
+  file.puts("#{name},#{created},#{completed}")
+end
